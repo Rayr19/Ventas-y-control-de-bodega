63e798b4acd28f754eb7a6557b0495e5e0d44674
Farid Fernandez <fernandezgfarid@gmail.com>
2026-06-07 01:38:17 -0500

    Reporte general

--- VENTAS Y GESTION.py
+++ VENTAS Y GESTION.py
@@ -39,3 +39,16 @@ def agregar_producto(nombres, precios, stock):
         print("Registro válido: Producto agregado correctamente.")
     else:
         print("Error: Capacidad máxima de almacén alcanzada.")
+
+def generar_reporte(h_ventas, h_montos, h_deudores):
+    print("\n--- REPORTE DE HISTORICOS (Ventas y Cuentas por Cobrar) ---")
+    if len(h_ventas) == 0:
+        print("No hay ventas registradas en el sistema.")
+    else:
+        for i in range(len(h_ventas)):
+            if h_deudores[i] == "Efectivo":
+                print("Venta N", i + 1, " -> Producto:", h_ventas[i], " | Monto: S/.", h_montos[i], " | Estado: PAGADO (Efectivo)")
+            else:
+                print("Venta N", i + 1, " -> Producto:", h_ventas[i], " | Monto: S/.", h_montos[i], " | Estado: POR COBRAR a [", h_deudores[i], "]")
+        print("Reporte correcto")
+                
